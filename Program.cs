70ba478d452df78b8bac8c061dad5d7227121aec
Daniel Klem <dklem90@gmail.com>
2021-05-21 18:57:21 -0500

    added Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace playing_cards_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
