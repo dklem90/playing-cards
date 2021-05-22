309160e3cbc71631b4179b45c2abdc46e463588d
Daniel Klem <dklem90@gmail.com>
2021-05-22 09:08:11 -0500

    Added DealerService.cs

---
+++ Services/DealerService.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+
+using PlayingCardsApp.Models;
+
+namespace PlayingCardsApp.Services
+{
+    public class DealerService
+    {
+        public DealerService() { }
+
+        public void CreateDeck()
+        {
+
+        }
+
+        public void ShuffleDeck(List<Card> cardDeck)
+        {
+
+        }
+    }
+}
