5ecf8c2697b7d98438d1100e9da268550eaae4fc
dklem90 <>
2021-05-22 02:06:00 +0000

    Added Card.cs and Heart.cs

---
+++ Models/Card.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PlayingCardsApp.Models
+{
+    public abstract class Card
+    {
+        public string Suit;
+        public string Value;
+
+        public Card() {}
+    }
+}
---
+++ Models/Heart.cs
@@ -0,0 +1,16 @@
+using System;
+
+using PlayingCardsApp.Models;
+
+namespace PlayingCardsApp.Models
+{
+    public class Heart : Card
+    {
+        suit = "HEART";
+
+        public Heart()
+        {
+
+        }
+    }
+}
